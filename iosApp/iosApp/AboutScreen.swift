3c5c986d6cd544639206b399cd6219ed98e1d4f4
Hernan Provoste <hernanprovostev@outlook.com>
2024-08-27 11:37:11 -0400

    Initial commit

---
+++ iosApp/iosApp/AboutScreen.swift
@@ -0,0 +1,21 @@
+//
+//  AboutScreen.swift
+//  iosApp
+//
+//  Created by Hernán Provoste Venegas on 20-08-24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutScreen: View {
+    var body: some View {
+        NavigationStack {
+            AboutScreen().navigationTitle("About Device")
+        }
+    }
+}
+
+#Preview {
+    AboutScreen()
+}
